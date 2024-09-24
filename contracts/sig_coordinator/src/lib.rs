66a425cda22128be241becd37ea70c223d5372ff
themicp <themicp@gmail.com>
2024-09-24 11:10:25 +0000

    Create signing coordinator smart contract

---
+++ contracts/sig_coordinator/src/lib.rs
@@ -0,0 +1,79 @@
+mod bls;
+mod msg;
+mod state;
+
+use std::collections::HashMap;
+
+use crate::msg::QueryMsg;
+use bls::Session;
+use cosmwasm_std::{
+    entry_point, to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response,
+    StdResult,
+};
+use msg::ExecuteMsg;
+use state::{SESSION, SESSION_COUNTER};
+
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: Empty,
+) -> StdResult<Response> {
+    SESSION.save(deps.storage, &HashMap::new())?;
+    SESSION_COUNTER.save(deps.storage, &0)?;
+    Ok(Response::new())
+}
+
+#[entry_point]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    msg: ExecuteMsg,
+) -> StdResult<Response> {
+    match msg {
+        ExecuteMsg::CreateSession {} => {
+            let mut counter = SESSION_COUNTER.load(deps.storage)?;
+            counter += 1;
+            let session_id = counter.to_string();
+            let session = Session {
+                session_id: session_id.clone(),
+                sigs: vec![],
+            };
+
+            let mut sessions = SESSION.load(deps.storage)?;
+            sessions.insert(session_id.clone(), session);
+            SESSION.save(deps.storage, &sessions)?;
+            SESSION_COUNTER.save(deps.storage, &counter)?;
+
+            Ok(Response::new()
+                .add_attribute("action", "create_session")
+                .add_attribute("session_id", session_id))
+        }
+        ExecuteMsg::PostPartialSig {
+            session_id,
+            partial_sig,
+        } => {
+            let mut sessions = SESSION.load(deps.storage)?;
+
+            let session = sessions.get_mut(&session_id).unwrap();
+            session.sigs.push(partial_sig);
+
+            SESSION.save(deps.storage, &sessions)?;
+
+            Ok(Response::new().add_attribute("action", "post_partial_sig"))
+        }
+    }
+}
+
+#[entry_point]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    let sessions = SESSION.load(deps.storage)?;
+    match msg {
+        QueryMsg::Session { session_id } => to_json_binary(&sessions.get(&session_id).unwrap()),
+        QueryMsg::PartialSigs { session_id } => {
+            to_json_binary(&sessions.get(&session_id).unwrap().sigs)
+        }
+    }
+}
